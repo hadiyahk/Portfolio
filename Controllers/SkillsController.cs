c832b5c8507da3f4aa41a8f978da3890084da8e8
MartinPenaB <128414831+MartinPenaB@users.noreply.github.com>
2025-01-19 16:20:58 -0500

    some progress

---
+++ Controllers/SkillsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hportfolio.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
