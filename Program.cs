7baa30e0a1d17847a109f15549e1b035cc68df11
Hadiyah Khan <khan0465@algonquinlive.com>
2025-06-19 00:45:57 -0400

    changing_host_service

--- Program.cs
+++ Program.cs
@@ -1,24 +1,36 @@
-namespace Portfolio
+﻿namespace Portfolio
 {
     public class Program
     {
         public static void Main(string[] args)
+
+
         {
             var builder = WebApplication.CreateBuilder(args);
+
+         
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+            builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
             builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
             app.Run();
+
         }
     }
 }
